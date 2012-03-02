0925a1fc026082ec1ebfe423289d00cd14eb51ec
Vijay <vijay@hayek.(none)>
2012-03-01 17:46:41 -0800

    WorkQueue tools (work_info gives information for individual pieces of work and worker_status gives the latest status of running jobs)

---
+++ tools/work_info.php
@@ -0,0 +1,176 @@
+<html>
+<head>
+<link rel="stylesheet" href="blueprint/screen.css" type="text/css" media="screen, projection">
+
+
+</head>
+<body>
+<h2 align=center>Work Information</h2>
+
+<form action='/tools/work_info.php' method=get align=center>
+
+<input type='text' name='work' />
+<input type=submit value='search for work' />
+
+</form>
+<p>
+
+<?php
+
+$con = mysql_connect("localhost", "crawler", "daewoo");
+
+if (!$con) {
+   die('Error: could not connect. '. mysql_error());
+}
+
+mysql_select_db("WorkQueue", $con) or die('Error' . mysql_error());
+
+$where_clause = "true";
+if (isset($_GET["id"])) {
+  $where_clause = $where_clause." and id=".$_GET["id"];
+}
+if (isset($_GET["type"])) {
+  $where_clause = $where_clause." and type=".$_GET["type"];
+}
+
+if (isset($_GET["work"])) {
+  $where_clause = $where_clause." and strcmp(work, '".mysql_real_escape_string($_GET["work"])."')=0";
+}
+
+if (isset($_GET["company_id"])) {
+  $where_clause = $where_clause." and company_id=".$_GET["company_id"];
+}
+
+if (isset($_GET["worker_ip"]) && isset($_GET["worker_pid"])) {
+  $where_clause = $where_clause." and strcmp(worker_ip, '".$_GET["worker_ip"].
+    "')=0 and worker_pid=".$_GET["worker_pid"];
+}
+$sql = "SELECT id, work, type, company_id, frequency, output_server, output_database, worker_ip, worker_pid, created, started, completed, status, status_message from WorkQueue where ($where_clause) order by started desc limit 100";
+
+
+
+
+$result = mysql_query($sql);
+if (!$result) {
+   die('Error: ' .mysql_error());
+}
+
+
+$types["1"] = "Hub adder";
+$types["2"] = "Hub crawler";
+$types["3"] = "Deal crawler";
+$types["4"] = "Geo coder";
+$types["5"] = "Yelp reviewer";
+$types["6"] = "XML feed crawler";
+$types["7"] = "Restaurant.com yelp info gatherer";
+$types["8"] = "Restaurant.com feed crawler";
+$types["101"] = "Cache reloader";
+$types["102"] = "Solr rebuilder";
+$types["103"] = "Email sender";
+$types["201"] = "Work reaper";
+$types["202"] = "Worker restarter";
+
+$companies["1"] = "Groupon";
+$companies["2"] = "LivingSocial";
+$companies["3"] = "BuyWithMe";
+$companies["4"] = "Tippr";
+$companies["5"] = "Travel Zoo";
+$companies["6"] = "Angie's List";
+$companies["7"] = "Gilt City";
+$companies["8"] = "Yollar ";
+$companies["9"] = "Zozi";
+$companies["10"] = "Bloomspot";
+$companies["11"] = "ScoutMob";
+$companies["12"] = "Amazon Local";
+$companies["13"] = "KGB Deals";
+$companies["14"] = "LifeBooker";
+$companies["15"] = "DealOn";
+$companies["16"] = "EverSave";
+$companies["17"] = "LS Escapes";
+$companies["18"] = "Google Offers";
+$companies["19"] = "GetMyPerks";
+$companies["20"] = "Voice Daily Deals";
+$companies["21"] = "Munch on Me";
+$companies["22"] = "Doodle Deals";
+$companies["23"] = "Juice in the City";
+$companies["24"] = "Schwaggle";
+$companies["25"] = "Home Run";
+$companies["26"] = "Bargain Bee";
+$companies["27"] = "SignPost";
+$companies["30"] = "DealFind";
+$companies["31"] = "Restaurant.com";
+$companies["32"] = "Pinchit";
+$companies["33"] = "GoldStar Events";
+$companies["34"] = "OnSale";
+$companies["35"] = "LivingSocial Adventures";
+$companies["36"] = "Entertainment.com";
+$companies["37"] = "Thrillist";
+$companies["38"] = "Savored";
+$companies["39"] = "MSN Offers";
+$companies["40"] = "CBS Local Offers";
+$companies["41"] = "CrowdSavings";
+
+
+
+
+
+echo "<table>\n";
+echo "     <tr>\n";
+echo "        <td><b>ID</b></td>\n";
+echo "        <td><b>Work</b></td>\n";
+echo "        <td><b>Type</b></td>\n";
+echo "        <td><b>Company_ ID</b></td>\n";
+echo "        <td><b>Frequency</b></td>\n";
+echo "        <td><b>Worker IP</b></td>\n";
+echo "        <td><b>PID</b></td>\n";
+echo "        <td><b>Created</b></td>\n";
+echo "        <td><b>Started</b></td>\n";
+echo "        <td><b>Completed</b></td>\n";
+echo "        <td><b>Status</b></td>\n";
+echo "        <td><b>Status Message</b></td>\n";
+echo "     </tr>\n";
+
+for ($i=0;$i < mysql_num_rows($result); $i++) {
+  $id = mysql_result($result, $i, "id");
+  $work = mysql_result($result, $i, "work");
+  $type = mysql_result($result, $i, "type");
+  $company_id = mysql_result($result, $i, "company_id");
+  $frequency = mysql_result($result, $i, "frequency");
+  $output_server = mysql_result($result, $i, "output_server");
+  $output_database = mysql_result($result, $i, "output_database");
+  $worker_ip = mysql_result($result, $i, "worker_ip");
+  $worker_pid = mysql_result($result, $i, "worker_pid");
+  $created = mysql_result($result, $i, "created");
+  $started = mysql_result($result, $i, "started");
+  $completed = mysql_result($result, $i, "completed");
+  $status = mysql_result($result, $i, "status");
+  $status_message = mysql_result($result, $i, "status_message");
+
+
+
+  echo "     <tr>\n";
+  echo "        <td>$id</td>\n";
+  echo "        <td><a href='/tools/work_info.php?work=$work' target=_work_info>$work</a> (<a href='$work' target=_blank>web</a>) (<a href='http://50.57.43.108/tools/deal_info.php?submiturl&deal_url=".htmlentities($work)."' target=_deal_info>db</a>)</td>\n";
+  echo "        <td><a href='/tools/work_info.php?type=$type' target=_work_info>$types[$type]</a></td>\n";
+  echo "        <td><a href='/tools/work_info.php?company_id=$company_id' target=_work_info>$companies[$company_id] ($company_id)</a></td>\n";
+
+  echo "        <td>$frequency</td>\n";
+  echo "        <td><a href='/tools/work_info.php?worker_ip=$worker_ip&worker_pid=$worker_pid' target=_work_info>$worker_ip</a></td>\n";
+  echo "        <td><a href='/tools/work_info.php?worker_ip=$worker_ip&worker_pid=$worker_pid' target=_work_info>$worker_pid</a></td>\n";
+
+  echo "        <td>$created</td>\n";
+  echo "        <td>$started</td>\n";
+  echo "        <td>$completed</td>\n";
+  echo "        <td>$status</td>\n";
+  echo "        <td>$status_message</td>\n";
+  echo "     </tr>\n";
+}
+
+echo "</table>\n";
+
+echo "$sql<BR>\n";
+?>
+
+
+</body>
+</html>
---
+++ tools/worker_status.php
@@ -0,0 +1,185 @@
+<html>
+<head>
+<link rel="stylesheet" href="blueprint/screen.css" type="text/css" media="screen, projection">
+
+</head>
+<body>
+
+<?php
+
+$con = mysql_connect("localhost", "crawler", "daewoo");
+
+if (!$con) {
+   die('Error: could not connect. '. mysql_error());
+}
+
+mysql_select_db("WorkQueue", $con) or die('Error' . mysql_error());
+
+
+$operation_sql = "";
+$where_clause = "";
+
+$operation_sql = "";
+if (isset($_POST["force_shutdown"])) {
+  $operation = "force_shutdown";
+  $operation_sql = "UPDATE Workers set force_shutdown=1 WHERE ";
+} else if (isset($_POST["clear_shutdown"])) {
+  $operation = "clear_shutdown";
+  $operation_sql ="DELETE FROM Workers WHERE ";
+} else if (isset($_POST["clear_zombies"])) {
+  $operation = "clear_zombies";
+  $operation_sql ="DELETE FROM Workers WHERE ";
+}
+
+foreach ($_POST as $key => $value) {
+  //echo "$key ** $value\n";
+
+  $info = preg_split("/:/", $key);
+  if (count($info)==3) {
+    if (strcmp($info[0], $operation)==0) {
+      $ip = str_replace("_", ".", $info[1]);
+      $pid = $info[2];
+      if (strcmp($where_clause, "")==0) {
+	$where_clause = "(strcmp(ip, '$ip')=0 and pid=$pid)";
+      } else {
+	$where_clause =
+	  $where_clause." or (strcmp(ip, '$ip')=0 and pid='$pid')";
+      }
+    }
+  }
+}
+
+$operation_sql = $operation_sql.$where_clause;
+if (strcmp($where_clause, "")!= 0) {
+  echo "$operation_sql<BR>\n";
+
+  $result = mysql_query($operation_sql);
+  if (!$result) {
+    die('Error: ' .mysql_error());
+  }
+}
+
+$sql = "SELECT ip, pid, type, status, latest_work_id, force_shutdown, TIME_TO_SEC(TIMEDIFF(UTC_TIMESTAMP(), heartbeat)), TIME_TO_SEC(TIMEDIFF(UTC_TIMESTAMP(), spawned)) FROM Workers order by type";
+
+$result = mysql_query($sql);
+if (!$result) {
+   die('Error: ' .mysql_error());
+}
+
+//echo "$sql\n";
+$types["1"] = "Hub adder";
+$types["2"] = "Hub crawler";
+$types["3"] = "Deal crawler";
+$types["4"] = "Geo coder";
+$types["5"] = "Yelp reviewer";
+$types["6"] = "XML feed crawler";
+$types["7"] = "Restaurant.com yelp info gatherer";
+$types["8"] = "Restaurant.com feed crawler";
+$types["9"] = "Geo fixer";
+$types["101"] = "Cache reloader";
+$types["102"] = "Solr rebuilder";
+$types["103"] = "Email sender";
+$types["201"] = "Work reaper";
+$types["202"] = "Worker restarter";
+
+$machines["108.166.127.22"] = "crawler1";
+$machines["184.106.175.144"] = "crawler2";
+$machines["184.106.174.162"] = "crawler3";
+$machines["50.57.36.164"] = "crawler4";
+
+
+echo "<h2 align=center>Worker Status</h2>\n";
+echo "<h4 align=center>".mysql_num_rows($result)." jobs</h3>\n";
+echo "<p>\n";
+echo "<form action='/tools/worker_status.php' method=post>";
+echo "<table>\n";
+echo "     <tr>\n";
+echo "        <td><b>IP</b></td>\n";
+echo "        <td><b>PID</b></td>\n";
+echo "        <td><b>Status</b></td>\n";
+echo "        <td><b>TYPE</b></td>\n";
+echo "        <td><b>Latest work ID</b></td>\n";
+echo "        <td><b>Uptime (hours)</b></td>\n";
+echo "        <td><b>Time since last heartbeat</b></td>\n";
+echo "        <td><b>Force shutdown of process</b></td>\n";
+echo "     </tr>\n";
+
+for ($i=0;$i < mysql_num_rows($result); $i++) {
+    $ip = mysql_result($result, $i, "ip");
+    $pid = mysql_result($result, $i, "pid");
+    $type = mysql_result($result, $i, "type");
+    $status = mysql_result($result, $i, "status");
+    $force_shutdown = mysql_result($result, $i, "force_shutdown");
+
+
+    $latest_work_id = mysql_result($result, $i, "latest_work_id");
+    $last_heartbeat = mysql_result($result, $i, "TIME_TO_SEC(TIMEDIFF(UTC_TIMESTAMP(), heartbeat))");
+    $uptime = mysql_result($result, $i, "TIME_TO_SEC(TIMEDIFF(UTC_TIMESTAMP(), spawned))");
+    $uptime = round($uptime / 3600,3);
+
+
+    echo "     <tr>\n";
+    echo "        <td>$machines[$ip] ($ip)</td>\n";
+    echo "        <td>$pid</td>\n";
+    if ($status) {
+      echo "        <td><span style='color:green'>running</span></td>\n";
+    } else {
+      echo "        <td><span style='color:red'>shutdown</span><input type='hidden' name='clear_shutdown:$ip:$pid'  /></td>\n";
+    }
+    echo "        <td>$types[$type] ($type)</td>\n";
+    echo "        <td><a href='/tools/work_info.php?id=$latest_work_id' target=_work_info>$latest_work_id</a></td>\n";
+    echo "        <td>$uptime</td>\n";
+    if ($last_heartbeat >1000) {
+      $color = "red";
+    } else {
+      $color = "green";
+    }
+    echo "        <td><span style='color:$color'>$last_heartbeat</span></td>\n";
+    if ($force_shutdown) {
+      echo "        <td><span style='color:red'>Shutdown requested</span><input type='hidden' name='clear_zombies:$ip:$pid'  /></td>\n";
+    } else {
+      echo "        <td><input type='checkbox' name='force_shutdown:$ip:$pid'  /></td>\n";
+    }
+    echo "     </tr>\n";
+}
+
+echo "     <tr>\n";
+echo "        <td> </td>\n";
+echo "        <td></td>\n";
+echo "        <td></td>\n";
+echo "        <td></td>\n";
+echo "        <td></td>\n";
+echo "        <td></td>\n";
+echo "        <td></td>\n";
+echo "        <td><input type=submit name='force_shutdown' value='Shutdown selected jobs' /></td>\n";
+echo "     </tr>\n";
+
+echo "     <tr>\n";
+echo "        <td> </td>\n";
+echo "        <td></td>\n";
+echo "        <td></td>\n";
+echo "        <td></td>\n";
+echo "        <td></td>\n";
+echo "        <td></td>\n";
+echo "        <td></td>\n";
+echo "        <td><input type=submit name='clear_shutdown' value='Clear shutdown jobs' /></td>\n";
+echo "     </tr>\n";
+
+echo "     <tr>\n";
+echo "        <td> </td>\n";
+echo "        <td></td>\n";
+echo "        <td></td>\n";
+echo "        <td></td>\n";
+echo "        <td></td>\n";
+echo "        <td></td>\n";
+echo "        <td></td>\n";
+echo "        <td><input type=submit name='clear_zombies' value='Clear zombie jobs' onclick=\"javascript:return confirm('Are you sure? Only do this if you have already requested a shutdown and believe you have zombie jobs')\" /></td>\n";
+echo "     </tr>\n";
+
+echo "</table>\n";
+echo "</form>\n";
+?>
+
+
+</body>
+</html>
