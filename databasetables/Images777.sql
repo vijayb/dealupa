8edf1ed45ffe1093ca00609e77c7f64a16f11071
Vijay <vijay@hayek.(none)>
2012-03-13 16:34:19 -0700

    Small change to images table to mark when an image_url has been downloaded, processed with ImageMagick, and uploaded to Amazon s3

--- databasetables/Images777.sql
+++ databasetables/Images777.sql
@@ -30,11 +30,12 @@ CREATE TABLE IF NOT EXISTS `Images777` (
   `deal_id` int(10) unsigned NOT NULL,
   `image_url` varchar(2000) NOT NULL,
   `image_url_hash` binary(20) NOT NULL,
-  `s3_image_url` varchar(200),
+  `on_s3` boolean not null default false,
   PRIMARY KEY (`deal_id`,`image_url_hash`),
   UNIQUE KEY `id` (`id`),
   KEY `deal_id` (`deal_id`),
-  KEY `image_url_hash` (`image_url_hash`)
+  KEY `image_url_hash` (`image_url_hash`),
+  KEY `on_s3` (`on_s3`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=116589 ;
 
 --
