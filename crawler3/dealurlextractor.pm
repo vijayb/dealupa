8022e8647d2d932650d9802d308e5e457addc720
Vijay <vijay@hayek.(none)>
2012-08-08 14:07:28 -0700

    Fixed URL extraction for schwaggle

--- crawler3/dealurlextractor.pm
+++ crawler3/dealurlextractor.pm
@@ -573,10 +573,10 @@
         my @deal_urls = ${$tree_ref}->look_down(
             sub{$_[0]->tag() eq 'a' &&
 		    defined($_[0]->attr('href')) &&
-		    $_[0]->attr('href') =~ /^http:\/\/schwaggle.active.com\/deal\//});
+		    $_[0]->attr('href') =~ /^\/deal\//});
 
 	foreach my $deal_url (@deal_urls) {
-	    my $clean_url = $deal_url->attr('href');
+	    my $clean_url = "http://schwaggle.active.com".$deal_url->attr('href');
 	    # Schwaggle postpends the city to the url. E.g.,:
 	    # http:// ... deal url ... /seattle
 	    # We get rid of the end so we don't have to crawl
